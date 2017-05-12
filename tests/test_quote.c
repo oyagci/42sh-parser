f2d46afd50ee8b142b454a87e6a4f628018c1bb4
Oguzhan YAGCI <oyagci@student.42.fr>
2017-05-12 17:30:54 +0200

    Add tests

--- tests/test_quote.c
+++ tests/test_quote.c
@@ -6,7 +6,7 @@
 /*   By: oyagci <oyagci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/12 17:15:43 by oyagci            #+#    #+#             */
-/*   Updated: 2017/05/12 17:21:10 by oyagci           ###   ########.fr       */
+/*   Updated: 2017/05/12 17:30:35 by oyagci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,39 @@ START_TEST(lexer_quote_quote)
 }
 END_TEST
 
+START_TEST(lexer_quote_doublequote)
+{
+	t_token	tokens[] = {
+		{ T_WORD, "\"Hello World\"" },
+		{ T_END, NULL }
+	};
+
+	test_expected_tokens("\"Hello World\"", tokens, sizeof(tokens) / sizeof(t_token));
+}
+END_TEST
+
+START_TEST(lexer_quote_backslash)
+{
+	t_token	tokens[] = {
+		{ T_WORD, "Hello\\ World" },
+		{ T_END, NULL }
+	};
+
+	test_expected_tokens("Hello\\ World", tokens, sizeof(tokens) / sizeof(t_token));
+}
+END_TEST
+
+START_TEST(lexer_quote_backslash_multiple)
+{
+	t_token	tokens[] = {
+		{ T_WORD, "Hello\\ World\\ Nice" },
+		{ T_END, NULL }
+	};
+
+	test_expected_tokens("Hello\\ World\\ Nice", tokens, sizeof(tokens) / sizeof(t_token));
+}
+END_TEST
+
 Suite	*test_suite_lexer_quote(void)
 {
 	Suite *s;
@@ -34,6 +67,9 @@ Suite	*test_suite_lexer_quote(void)
 	s = suite_create("Lexer Quote");
 	tc_quote = tcase_create("Core");
 	tcase_add_test(tc_quote, lexer_quote_quote);
+	tcase_add_test(tc_quote, lexer_quote_doublequote);
+	tcase_add_test(tc_quote, lexer_quote_backslash);
+	tcase_add_test(tc_quote, lexer_quote_backslash_multiple);
 	suite_add_tcase(s, tc_quote);
 	return (s);
 }
