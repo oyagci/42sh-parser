771a48318fa5de8c10587723e6d573051a4250fa
Oguzhan YAGCI <oyagci@student.42.fr>
2017-05-18 14:50:16 +0200

    Add tests to test_filename.c

--- tests/sources/parse_tree/filename/test_filename.c
+++ tests/sources/parse_tree/filename/test_filename.c
@@ -6,7 +6,7 @@
 /*   By: oyagci <oyagci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/17 16:13:04 by oyagci            #+#    #+#             */
-/*   Updated: 2017/05/18 11:01:29 by oyagci           ###   ########.fr       */
+/*   Updated: 2017/05/18 14:48:39 by oyagci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,29 @@ START_TEST(test_filename)
 	t_ptree	*fname = filename(&p);
 	ck_assert(fname != NULL);
 
-	ck_assert_str_eq(fname->content.filename->data, "hello");
+	ck_assert_str_eq(fname->content->filename.data, "hello");
 	ck_assert(((t_token *)p.tlst->content)->type == T_END);
+}
+END_TEST
+
+START_TEST(test_filename_two)
+{
+	t_token	tarr[] = {
+		{ T_WORD, ft_strdup("hello") },
+		{ T_WORD, ft_strdup("World") },
+		{ T_END, NULL },
+	};
+	t_list	*lst = get_tlst(tarr, 3);
+	t_parser p = {
+		lst,
+		NULL,
+	};
+
+	t_ptree	*fname = filename(&p);
+	ck_assert(fname != NULL);
+
+	ck_assert_str_eq(fname->content->filename.data, "hello");
+	ck_assert(((t_token *)p.tlst->content)->type == T_WORD);
 }
 END_TEST
 
@@ -50,7 +71,25 @@ START_TEST(test_filename_tilde_expansion)
 	t_ptree	*fname = filename(&p);
 	ck_assert(fname != NULL);
 
-	ck_assert_str_eq(fname->content.filename->data, "/Users/oyagci");
+	ck_assert_str_eq(fname->content->filename.data, "/Users/oyagci");
+}
+END_TEST
+
+START_TEST(test_filename_syntax_error)
+{
+	t_token	tarr[] = {
+		{ T_LESS, ft_strdup("<") },
+		{ T_END, NULL },
+	};
+	t_list	*lst = get_tlst(tarr, 2);
+	t_parser p = {
+		lst,
+		NULL,
+	};
+
+	t_ptree	*fname = filename(&p);
+	ck_assert(fname == NULL);
+	ck_assert(((t_token *)p.tlst->content)->type == T_LESS);
 }
 END_TEST
 
@@ -62,6 +101,8 @@ Suite	*test_suite_filename()
 	s = suite_create("filename");
 	tc = tcase_create("core");
 	tcase_add_test(tc, test_filename);
+	tcase_add_test(tc, test_filename_two);
+	tcase_add_test(tc, test_filename_syntax_error);
 	tcase_add_test(tc, test_filename_tilde_expansion);
 	suite_add_tcase(s, tc);
 	return (s);
